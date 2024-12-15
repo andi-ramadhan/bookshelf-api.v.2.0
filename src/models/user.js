3a41317e8f066731b1163523a16316b23e23440d
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2024-12-15 18:18:27 +0700

    Add auth: In Progress

---
+++ src/models/user.js
@@ -0,0 +1,40 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+const bcrypt = require('bcrypt');
+const { nanoid } = require('nanoid');
+
+const User = sequelize.define('users', {
+  id: {
+    type: DataTypes.STRING,
+    defaultValue: () => nanoid(),
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull:false,
+    validate: {
+      len: {
+        args: [8, 255],
+        msg: "Password must be at least 8 characters long"
+      }
+    }
+  },
+  role: {
+    type: DataTypes.STRING,
+    defaultValue: 'user',
+    allowNull: false,
+  }
+}, {
+  hooks: {
+    beforeCreate: async (user) => {
+      user.password = await bcrypt.hash(user.password, 10);
+    }
+  }
+});
+
+module.exports = User;
---
+++ src/routes/auth.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const { nanoid } = require('nanoid');
+const bcrypt = require('bcrypt');
+
+const router = express.Router();
+
+router.post('/signup', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.create({ id: nanoid(), username, password });
+    res.status(201).json({ message: 'User created successfully '});
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ where: { username } });
+
+    if(!user) {
+      return res.status(401).json({ message: 'Invalid username or password' });
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+      return res.status(401).json({ message: 'Invalid username or password' });
+    }
+
+    const token = jwt.sign({ userId: user.id, role: user.role }, process.env.JWT_SECRET, {expiresIn: '1h'});
+
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
